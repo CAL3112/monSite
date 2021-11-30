50900bdc57ebdbc77bbca3304770126a97f3a495
CAL3112 <lucas.michel52240@gmail.com>
2021-11-30 10:32:17 +0100

    ajout projets figma

--- projets.php
+++ projets.php
@@ -13,17 +13,20 @@
     <div class="div-lien-projet"><a class="lien-projet" href="projets/SiteCatherine/index.html" target="_blank">Voir le site</a></div>
 </div>
 
-<!-- <div class="div-projet">
+<div class="div-projet">
     <div class="div-text-projet">
         <p>
             <b>Réalisation d'une maquette pour un club de handball en utilisant le logiciel Figma</b><br><br>
             <i>L'objectif de ce projet de graphisme était de créer une maquette de site internet en utilisant le logiciel Figma. J'ai pu, lors de ce projet, perfectionner ma connaissance du logiciel Figma ainsi que renforcer les acquis en compétences de graphismes.<br><br>(09/2021)</i>
         </p>
     </div>
-    <div class="div-lien-projet"><a class="lien-projet" href="" target="_blank">Vidéo de démonstration</a>
-    </div> 
-
-</div> -->
+    <div class="div-colonne">
+    <div class="div-lien-projet2"><a class="lien-projet" href="https://www.figma.com/proto/YnXJAQrwOhJui65cLOUIIO/PRINCIPALE?node-id=0%3A1&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=407%3A8552" target="_blank">Maquette PC</a>
+    </div>
+    <div class="div-lien-projet2"><a class="lien-projet" href="https://www.figma.com/proto/YnXJAQrwOhJui65cLOUIIO/PRINCIPALE?node-id=385%3A332&scaling=scale-down&page-id=357%3A200&starting-point-node-id=412%3A2028" target="_blank">Maquette Mobile</a>
+    </div>
+    </div>
+</div>
 
 <div class="div-projet">
     <div class="div-text-projet">
@@ -44,6 +47,7 @@
     </div>
         <div class="div-lien-projet"><a class="lien-projet" href="projets/GestionDesCochon/index.php" target="_blank">Gestion des cochons</a></div>
 </div>
+
 
 <h1>D'autres projets sont en cours <i class="fas fa-sync-alt loading"></i></i></h1>
 
